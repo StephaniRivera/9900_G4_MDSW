4035fb4df4eb9709a04a2044c88711b78f8889fe
sebastianqp <120067165+sebastianqp@users.noreply.github.com>
2023-07-23 19:44:14 -0500

    Oferta Correcion de errores

--- CODE/Oferta.js
+++ CODE/Oferta.js
@@ -44,6 +44,45 @@ function mostrarOfertaSiguiente() {
 
   mostrarOferta(indiceOfertaActual);
 }
+// Supongamos que tenemos el siguiente array de ofertas (puedes ajustar los valores según tus necesidades)
+var ofertas = [
+  { titulo: "Oferta 1", descripcion: "Descripción de la oferta 1", descuento: 10 },
+  { titulo: "Oferta 2", descripcion: "Descripción de la oferta 2", descuento: 20 },
+  // ... más ofertas ...
+  { titulo: "Oferta N", descripcion: "Descripción de la oferta N", descuento: 30 }
+];
+
+// Supongamos que la variable indiceOfertaActual tiene un valor inicial
+var indiceOfertaActual = 0;
+
+// Definimos la función mostrarOferta que mostrará la oferta en el índice dado
+function mostrarOferta(indice) {
+  var oferta = ofertas[indice];
+  console.log("Oferta:", oferta.titulo);
+  console.log("Descripción:", oferta.descripcion);
+  console.log("Descuento:", oferta.descuento + "%");
+}
+
+// Función mostrarOfertaSiguiente que mostrará la siguiente oferta en la lista
+function mostrarOfertaSiguiente() {
+  if (indiceOfertaActual === ofertas.length - 10) {
+    indiceOfertaActual = 0;
+  } else {
+    indiceOfertaActual++;
+  }
+
+  mostrarOferta(indiceOfertaActual);
+}
+
+// Mostrar la oferta actual
+console.log("Oferta actual:");
+mostrarOferta(indiceOfertaActual);
+
+// Mostrar la siguiente oferta utilizando un ciclo for (en este caso, mostraremos las próximas 5 ofertas)
+console.log("Próximas 5 ofertas:");
+for (var i = 0; i < 5; i++) {
+  mostrarOfertaSiguiente();
+}
 
 function mostrarOfertaAnterior() {
   if (indiceOfertaActual === 0) {
