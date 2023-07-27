0445023cfec68dd9ae3cd7a5e1d478cc04e4c8cf
sebastianqp <120067165+sebastianqp@users.noreply.github.com>
2023-07-26 20:58:26 -0500

    Update Contacto.js

--- CODE/Contacto.js
+++ CODE/Contacto.js
@@ -11,6 +11,38 @@ function showCompanyInfo() {
   console.log("Gmail: " + email);
   console.log(instagramMessage);
 }
+// Información de la empresa
+const companyName = "Dolce Peccato";
+const contactMessage = "Contacta con nosotros";
+const email = "anahi_suquillom@outlook.com";
+const instagramMessage = "Encuéntranos en Instagram como: amsdolce_peccato";
+
+// Función para mostrar la información
+function showCompanyInfo() {
+  console.log("Nombre de la empresa: " + companyName);
+  console.log(contactMessage);
+  
+  // Validar si el email contiene el símbolo @
+  if (email.includes('@')) {
+    console.log("Gmail: " + email);
+    console.log(instagramMessage);
+  } else {
+    console.log("El email no es válido.");
+  }
+}
+
+// Función para redireccionar a Instagram si el email es válido
+function redirectToInstagram() {
+  if (email.includes('@')) {
+    // Reemplaza el enlace con tu URL de Instagram
+    window.location.href = "https://www.instagram.com/amsdolce_peccato/";
+  } else {
+    console.log("El email no es válido para acceder a Instagram.");
+  }
+}
+
+// Mostrar la información de la empresa al cargar la página
+showCompanyInfo();
 
 // Función para redireccionar a Instagram
 function redirectToInstagram() {
